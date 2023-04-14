7e009052decee617485cf5a3de56c34dca2897a5
Nastya <simonovanastasia360@gmail.com>
2023-04-14 19:44:14 +0300

    feat: make remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,9 +1,32 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const { ListNode } = require('../extensions/list-node.js');
+//const { ListNode } = require('../extensions/list-node.js');
+
 
 function removeKFromList(l, k) {
-  
+  while(l.value == k){ l = l.next;}
+  thisNode = l;
+  nextNode = thisNode.next;
+  while(nextNode != null)
+  {
+    if(nextNode.value == k)
+    {
+      if(thisNode!=null && nextNode.next != null)
+      {
+        if(nextNode.value == (nextNode.next).value) { thisNode.next =  (nextNode.next).next; }
+        else{ thisNode.next = nextNode.next; } 
+      }
+      else 
+      {
+        thisNode.next = null;
+        nextNode = null;
+        break;
+      }
+    }
+    thisNode = thisNode.next;
+    nextNode = thisNode.next;       
+  }
+  return l;
 }
 
 module.exports = {
