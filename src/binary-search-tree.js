469f5d7273175e68a446556c434a1d3b96b50cb4
Nastya <simonovanastasia360@gmail.com>
2023-04-14 12:50:12 +0300

    feat: make stack and queue

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,95 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 class BinarySearchTree {
 
+  constructor() {
+    this.treeRoot = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.treeRoot = addValue(this.treeRoot, data);
+    function addValue(node,value)
+    {
+      if (!node){ return new Node(value);}
+      if (node.value == value) {return node;}
+      if (value < node.value) { node.left = addValue(node.left, value);}
+      if (value > node.value) { node.right = addValue(node.right, value);}
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  has(data) {
+    return searchValue(this.treeRoot, data);
+    function searchValue(node, value)
+    {
+      if (!node){ return false;}
+      if (node.value === value) {return true;}
+      if (value < node.value) { return searchValue(node.left, value);}
+      if (value > node.value) { return searchValue(node.right, value);}
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return searchValue(this.treeRoot, data);
+    function searchValue(node, value)
+    {
+      if (!node){ return null;}
+      if (node.value === value) {return node;}
+      if (value < node.value) { return searchValue(node.left, value);}
+      if (value > node.value) { return searchValue(node.right, value);}
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.treeRoot = removeValue(this.treeRoot, data);
+    function removeValue(node,value)
+    {
+      if (!node){ return null;}
+      if (value < node.value) { node.left = removeValue(node.left, value); return node;}
+      else if (value > node.value) { node.right = removeValue(node.right, value); return node;}
+      else 
+      {
+        if (!node.left && !node.right) {return null;}
+        if (!node.left) {node = node.right; return node;}
+        if (!node.right) {node = node.left; return node;}
+
+        let minFromRight = node.right;
+        while(minFromRight.left)
+        {
+          minFromRight = minFromRight.left;
+        }
+        node.value = minFromRight.value;
+        node.right = removeValue(node.right, minFromRight.value);
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.treeRoot) {return null;}
+    let node = this.treeRoot;
+    while(node.left)
+    {
+      node = node.left;
+    }
+    return node.value;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.treeRoot) {return null;}
+    let node = this.treeRoot;
+    while(node.right)
+    {
+      node = node.right;
+    }
+    return node.value;
   }
 }
 
--- src/stack.js
+++ src/stack.js
@@ -1,31 +1,30 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor()
+  {
+    this.count = 0;
+    this.values = [];
+  }
+  
+  push(element) {
+    this.values[this.count] = element;
+    this.count++;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.count == 0){ return undefined;}
+    else{
+      this.count--;
+      let temp = this.values[this.count];
+      delete this.values[this.count];
+      return temp;
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.values[this.count-1];
   }
 }
 
